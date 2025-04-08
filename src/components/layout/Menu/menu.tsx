635a9fde6afe2fb1a6b4283505b56ac51fec8a62
Pantuzzo <icaropantuzzo@gmail.com>
2025-04-08 18:05:23 -0300

    refactor: Improve Menu component

--- src/components/layout/Menu/menu.tsx
+++ src/components/layout/Menu/menu.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
-import { Menu, Users, Clipboard, ClockAlert } from 'lucide-react';
+import { Menu, Users, Clipboard, ClockAlert, X } from 'lucide-react';
 
 export default function MobileMenu() {
   const [open, setOpen] = useState(false);
@@ -18,23 +18,35 @@ export default function MobileMenu() {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" onClick={() => setOpen(true)}>
-          <Menu className="h-6 w-6" />
+        <Button variant="ghost" className="group p-2 h-10 w-10">
+          <Menu className="h-6 w-6 text-white group-hover:text-slate-800" />
         </Button>
       </SheetTrigger>
 
-      <SheetContent side="left" className="w-64 p-4">
+      <SheetContent
+        side="left"
+        className="w-64 p-4 [&>button:first-of-type]:hidden"
+        style={{ backgroundColor: 'oklch(70.7% 0.022 261.325)' }}
+      >
         <Button
           variant="ghost"
           size="icon"
-          className="absolute right-4 top-4"
+          className="absolute right-4 top-4 text-white hover:text-black transition-colors p-1"
           onClick={closeMenu}
-        />
-
+        >
+          <X className="w-4 h-4" />
+        </Button>
         <nav className="mt-8 space-y-4">
+          <div className="flex items-center justify-center mb-4">
+            <img
+              src="https://agostinianas.com.br/wp-content/uploads/2020/12/logo-congregacao-branco.svg"
+              alt="Logo"
+              className="h-17 w-auto object-contain cursor-pointer"
+            />
+          </div>
           <Button
             variant="ghost"
-            className="w-full flex justify-start gap-2"
+            className="w-full flex justify-start gap-2 text-white"
             onClick={() => {
               navigate('/formulario-aluno');
               closeMenu();
@@ -45,7 +57,7 @@ export default function MobileMenu() {
           </Button>
           <Button
             variant="ghost"
-            className="w-full flex justify-start gap-2"
+            className="w-full flex justify-start gap-2 text-white"
             onClick={() => {
               navigate('/dashboard');
               closeMenu();
@@ -57,7 +69,7 @@ export default function MobileMenu() {
 
           <Button
             variant="ghost"
-            className="w-full flex justify-start gap-2"
+            className="w-full flex justify-start gap-2 text-white"
             onClick={() => {
               navigate('/dashboard/users');
               closeMenu();
@@ -68,7 +80,7 @@ export default function MobileMenu() {
 
           <Button
             variant="ghost"
-            className="w-full flex justify-start gap-2 text-red-500 h-5 p-5"
+            className="w-full flex justify-start gap-2 text-red-700 h-5 p-5"
             onClick={logOut}
           >
             Sair
