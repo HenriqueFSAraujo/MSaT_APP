9f5d8f8d3a20ae7c507952d0524575117b838479
Pantuzzo <icaropantuzzo@gmail.com>
2025-08-28 10:03:01 -0300

    fix: Adjusts of endpoints

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,24 +1,23 @@
 export const Endpoints = {
-    Users: {
-        List: '/users',
-        Profile: '/users/profile',
-        ResetPassword: `/users/reset-password`,
-        DisableUser: `/users/deactivate`,
-        Parecer: '/parecer-socioeconomico',
-        PDFParecer: '/gerar-parecer'
-    },
-    Auth: {
-        Login: '/auth/login',
-        Logout: '/auth/logout',
-    },
-    Forms: {
-        SchoolarShip_data: '/processo-bolsas',
-        Personal_Data: '/forms',
-        Parental_Data: '/parentes',
-        Address_Data: '/enderecos',
-        //TODO: Ajustar rota dos Docs
+  Users: {
+    List: '/users',
+    Profile: '/users/profile',
+    ResetPassword: `/users/reset-password`,
+    DisableUser: `/users/deactivate`,
+    Parecer: '/parecer-socioeconomico',
+    PDFParecer: '/gerar-parecer',
+  },
+  Auth: {
+    Login: '/auth/login',
+    Logout: '/auth/logout',
+  },
+  Forms: {
+    SchoolarShip_data: '/processo-bolsas',
+    Personal_Data: '/forms',
+    Parental_Data: '/parentes',
+    Address_Data: '/enderecos',
 
-        Housing_Data: '/form-condicoes-habitacionais',
-        Property_Data: '/bens-posses'
-    }
+    Housing_Data: '/form-condicoes-habitacionais',
+    Property_Data: '/bens-posses',
+  },
 } as const;
