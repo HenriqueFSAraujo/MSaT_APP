53998d309a881112313cbef76fe77a495d29ce2f
Pantuzzo <icaropantuzzo@gmail.com>
2025-09-12 13:57:44 -0300

    fix: Change label

--- src/components/AddressResidence/AddressResidence.tsx
+++ src/components/AddressResidence/AddressResidence.tsx
@@ -25,23 +25,23 @@ export const AddressResidence = ({ label }: { label: string }) => {
     resolver: zodResolver(addressInfoSchema),
     defaultValues: {
       address: '',
-    neighborhood: '',
-    city: '',
-    zipCode: '',
-    referencePoint: '',
-    residenceType: '',
-    structureType: '',
-    structureTypeOthers: '',
-    hasSewage: '',
-    electricitySource: '',
-    waterSupply: '',
-    transportType: '',
-    transportTypeOthers: '',
-    commutingTime: '',
-    afterSchoolActivities: '',
-    activityDescription: '',
-    weeklyFrequency: '',
-    ...(formData.address_info as Partial<AddressInfo>),
+      neighborhood: '',
+      city: '',
+      zipCode: '',
+      referencePoint: '',
+      residenceType: '',
+      structureType: '',
+      structureTypeOthers: '',
+      hasSewage: '',
+      electricitySource: '',
+      waterSupply: '',
+      transportType: '',
+      transportTypeOthers: '',
+      commutingTime: '',
+      afterSchoolActivities: '',
+      activityDescription: '',
+      weeklyFrequency: '',
+      ...(formData.address_info as Partial<AddressInfo>),
     },
   });
 
@@ -73,14 +73,14 @@ export const AddressResidence = ({ label }: { label: string }) => {
     }
   };
 
-   useEffect(() => {
-        if (data) {
-          methods.reset({
-            ...methods.getValues(),
-            ...(data as Partial<AddressInfo>),
-          });
-        }
-      }, [data, methods]);
+  useEffect(() => {
+    if (data) {
+      methods.reset({
+        ...methods.getValues(),
+        ...(data as Partial<AddressInfo>),
+      });
+    }
+  }, [data, methods]);
 
   const onSubmit = async (data: AddressInfo) => {
     const isValid = await methods.trigger();
@@ -92,7 +92,7 @@ export const AddressResidence = ({ label }: { label: string }) => {
     try {
       setFormData('address_info', data);
       setSelectedTab('required_documents');
-      
+
       // Garantindo que todos os campos obrigatórios estão presentes
       const payload = {
         userId: Number(StudentId),
@@ -112,13 +112,14 @@ export const AddressResidence = ({ label }: { label: string }) => {
         commutingTime: data.commutingTime,
         afterSchoolActivities: data.afterSchoolActivities,
         // Verificando se os campos condicionais são necessários
-        activityDescription: data.afterSchoolActivities === 'Sim' ? data.activityDescription : undefined,
-        weeklyFrequency: data.afterSchoolActivities === 'Sim' ? data.weeklyFrequency : undefined
-      }
-      
-      FormSubmit(payload)
+        activityDescription:
+          data.afterSchoolActivities === 'Sim' ? data.activityDescription : undefined,
+        weeklyFrequency: data.afterSchoolActivities === 'Sim' ? data.weeklyFrequency : undefined,
+      };
+
+      FormSubmit(payload);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
@@ -156,7 +157,7 @@ export const AddressResidence = ({ label }: { label: string }) => {
               />
               <FormSelect
                 name="residenceType"
-                label="O(a) candidato(a) reside:"
+                label="Situação do Imóvel:"
                 required
                 options={[
                   { value: 'Casa', label: 'Casa' },
@@ -166,123 +167,135 @@ export const AddressResidence = ({ label }: { label: string }) => {
                 ]}
                 error={errors.residenceType?.message}
               />
-            <FormSelect
-              name="structureType"
-              label="Estrutura fisica da moradia:"
-              required
-              options={[
-                { value: 'Alvenaria', label: 'Alvenaria' },
-                { value: 'Madeira', label: 'Madeira' },
-                { value: 'Taipa', label: 'Taipa' },
-                { value: 'Outros', label: 'Outros' },
-              ]}
-              withOtherOption={{
-                otherValue: "Outros",
-                otherFieldName: "structureTypeOthers",
-                otherPlaceholder: "Especifique..."
-              }}
-              error={errors.structureType?.message}
-            />
+              <FormSelect
+                name="structureType"
+                label="Estrutura fisica da moradia:"
+                required
+                options={[
+                  { value: 'Alvenaria', label: 'Alvenaria' },
+                  { value: 'Madeira', label: 'Madeira' },
+                  { value: 'Taipa', label: 'Taipa' },
+                  { value: 'Outros', label: 'Outros' },
+                ]}
+                withOtherOption={{
+                  otherValue: 'Outros',
+                  otherFieldName: 'structureTypeOthers',
+                  otherPlaceholder: 'Especifique...',
+                }}
+                error={errors.structureType?.message}
+              />
 
-            <FormSelect
-              name="hasSewage"
-              label="Possui Esgoto sanitário:"
-              required
-              options={[
-                { value: 'Existente', label: 'Existente' },
-                { value: 'Inexistente', label: 'Inexistente' },
-              ]}
-              error={errors.hasSewage?.message}
-            />
+              <FormSelect
+                name="hasSewage"
+                label="Possui Esgoto sanitário:"
+                required
+                options={[
+                  { value: 'Existente', label: 'Existente' },
+                  { value: 'Inexistente', label: 'Inexistente' },
+                ]}
+                error={errors.hasSewage?.message}
+              />
 
-            <FormSelect
-              name="electricitySource"
-              label="Fornecimento de Energia Elétrica:"
-              required
-              options={[
-                { value: 'Companhia distribuidora', label: 'Companhia distribuidora' },
-                { value: 'Inexistente', label: 'Inexistente' },
-              ]}
-              error={errors.electricitySource?.message}
-            />
+              <FormSelect
+                name="electricitySource"
+                label="Fornecimento de Energia Elétrica:"
+                required
+                options={[
+                  { value: 'Companhia distribuidora', label: 'Companhia distribuidora' },
+                  { value: 'Inexistente', label: 'Inexistente' },
+                ]}
+                error={errors.electricitySource?.message}
+              />
 
-            <FormSelect
-              name="waterSupply"
-              label="Abastecimento de Água:"
-              required
-              options={[
-                { value: 'Companhia distribuidora', label: 'Companhia distribuidora' },
-                { value: 'Inexistente', label: 'Inexistente' },
-              ]}
-              error={errors.waterSupply?.message}
-            />
+              <FormSelect
+                name="waterSupply"
+                label="Abastecimento de Água:"
+                required
+                options={[
+                  { value: 'Companhia distribuidora', label: 'Companhia distribuidora' },
+                  { value: 'Inexistente', label: 'Inexistente' },
+                ]}
+                error={errors.waterSupply?.message}
+              />
 
-            <FormSelect
-              name="transportType"
-              label="Utiliza transporte para chegar à Unidade Educacional:"
-              required
-              options={[
-                { value: 'Transporte público', label: 'Transporte público' },
-                { value: 'Transporte escolar', label: 'Transporte escolar' },
-                { value: 'Translado realizado pela família', label: 'Translado realizado pela família' },
-                { value: 'outros', label: 'Outros' },
-              ]}
-              withOtherOption={{
-                otherValue: "outros",
-                otherFieldName: "transportTypeOthers",
-                otherPlaceholder: "Especifique o tipo de transporte"
-              }}
-              error={errors.transportType?.message || errors.transportTypeOthers?.message}
-            />
+              <FormSelect
+                name="transportType"
+                label="Utiliza transporte para chegar à Unidade Educacional:"
+                required
+                options={[
+                  { value: 'Transporte público', label: 'Transporte público' },
+                  { value: 'Transporte escolar', label: 'Transporte escolar' },
+                  {
+                    value: 'Translado realizado pela família',
+                    label: 'Translado realizado pela família',
+                  },
+                  { value: 'outros', label: 'Outros' },
+                ]}
+                withOtherOption={{
+                  otherValue: 'outros',
+                  otherFieldName: 'transportTypeOthers',
+                  otherPlaceholder: 'Especifique o tipo de transporte',
+                }}
+                error={errors.transportType?.message || errors.transportTypeOthers?.message}
+              />
 
-             <FormSelect
-              name="commutingTime"
-              label="Tempo habitual gasto no deslocamento:"
-              required
-              options={[
-                { value: 'Até 10 minutos', label: 'Até 10 minutos' },
-                { value: 'Até 30 minutos', label: 'Até 30 minutos' },
-                { value: 'Até 1 hora', label: 'Até 1 hora' },
-                { value: 'Mais de 1 hora', label: 'Mais de 1 hora' },
-              ]}
-              error={errors.commutingTime?.message}
-            />
+              <FormSelect
+                name="commutingTime"
+                label="Tempo habitual gasto no deslocamento:"
+                required
+                options={[
+                  { value: 'Até 10 minutos', label: 'Até 10 minutos' },
+                  { value: 'Até 30 minutos', label: 'Até 30 minutos' },
+                  { value: 'Até 1 hora', label: 'Até 1 hora' },
+                  { value: 'Mais de 1 hora', label: 'Mais de 1 hora' },
+                ]}
+                error={errors.commutingTime?.message}
+              />
 
-            <FormSelect
-              name="afterSchoolActivities"
-              label="O(a) candidato(a) participa de atividades no contraturno escolar?"
-              required
-              options={[
-                { value: 'Não', label: 'Não' },
-                { value: 'Sim', label: 'Sim' },
-              ]}
-              error={errors.afterSchoolActivities?.message}
-            />
-            
-            {methods.watch('afterSchoolActivities') === 'Sim' && (
-              <>
-                <div className="col-span-1 sm:col-span-2 animate-in fade-in slide-in-from-left-5 duration-300">
-                  <FormInput
-                    name="activityDescription"
-                    label={<>Quais são as atividades que o(a) candidato(a) participa? <span className="text-red-500 ml-1">*</span></>}
-                    required
-                    error={errors.activityDescription?.message}
-                    withMarginTop
-                  />
-                </div>
-    
-                <div className="col-span-1 sm:col-span-2 animate-in fade-in slide-in-from-left-5 duration-300">
-                  <FormInput
-                    name="weeklyFrequency"
-                    label={<>Número de vezes por semana em que participa? <span className="text-red-500 ml-1">*</span></>}
-                    required
-                    error={errors.weeklyFrequency?.message}
-                    withMarginTop
-                  />
-                </div>
-              </>
-            )}
-              
+              <FormSelect
+                name="afterSchoolActivities"
+                label="O(a) candidato(a) participa de atividades no contraturno escolar?"
+                required
+                options={[
+                  { value: 'Não', label: 'Não' },
+                  { value: 'Sim', label: 'Sim' },
+                ]}
+                error={errors.afterSchoolActivities?.message}
+              />
+
+              {methods.watch('afterSchoolActivities') === 'Sim' && (
+                <>
+                  <div className="col-span-1 sm:col-span-2 animate-in fade-in slide-in-from-left-5 duration-300">
+                    <FormInput
+                      name="activityDescription"
+                      label={
+                        <>
+                          Quais são as atividades que o(a) candidato(a) participa?{' '}
+                          <span className="text-red-500 ml-1">*</span>
+                        </>
+                      }
+                      required
+                      error={errors.activityDescription?.message}
+                      withMarginTop
+                    />
+                  </div>
+
+                  <div className="col-span-1 sm:col-span-2 animate-in fade-in slide-in-from-left-5 duration-300">
+                    <FormInput
+                      name="weeklyFrequency"
+                      label={
+                        <>
+                          Número de vezes por semana em que participa?{' '}
+                          <span className="text-red-500 ml-1">*</span>
+                        </>
+                      }
+                      required
+                      error={errors.weeklyFrequency?.message}
+                      withMarginTop
+                    />
+                  </div>
+                </>
+              )}
             </div>
             <div className="flex justify-end w-full">
               <Button
